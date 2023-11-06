c559c0d0358b03061abb5e6ffa7082aeed45c1f1
luiscaceresd <luiscaceresd97@gmail.com>
2023-11-06 10:10:18 -0400

    updating

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,9 @@ import Information from "./Information";
 import Products from "./Products";
 
 // Import images
-import persona1 from "./assets/persona1.png";
-import persona2 from "./assets/persona2.png";
-import persona3 from "./assets/persona3.png";
-import persona4 from "./assets/persona4.png";
 import finalImage1 from "./assets/finalImage1.jpg";
 import finalImage2 from "./assets/finalImage2.jpg";
+import Testimonials from "./Testimonials";
 
 function Main() {
   return (
@@ -20,32 +17,7 @@ function Main() {
       <Products/>
 
       {/* Testimonials */}
-      <section>
-        <h2>Testimonials</h2>
-        <figure>
-          <figcaption>Lucas</figcaption>
-          <blockquote>The food was delicious!</blockquote>
-          <img src={persona1} alt="Lucas" />
-        </figure>
-
-        <figure>
-          <figcaption>Diana</figcaption>
-          <blockquote>Fast and yummy!</blockquote>
-          <img src={persona2} alt="Diana" />
-        </figure>
-
-        <figure>
-          <figcaption>Chris</figcaption>
-          <blockquote>Couldn't recommend it more.</blockquote>
-          <img src={persona3} alt="Chris" />
-        </figure>
-
-        <figure>
-          <figcaption>Matt</figcaption>
-          <blockquote>Really good food.</blockquote>
-          <img src={persona4} alt="Matt" />
-        </figure>
-      </section>
+      <Testimonials/>
 
       {/* Final section */}
       <section>
